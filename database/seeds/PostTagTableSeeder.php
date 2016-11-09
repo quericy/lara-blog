223662b7d36221709af2ecd7a8862df2d6cfcf02
quericy <quericy@live.com>
2016-11-09 11:35:14 +0800

    手动构造中间表post_tag的数据生成器

--- database/seeds/PostTagTableSeeder.php
+++ database/seeds/PostTagTableSeeder.php
@@ -11,6 +11,31 @@ class PostTagTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $posts_id_arr = DB::table('posts')->skip(0)->limit(5)->lists('id');
+        $tags_id_arr = DB::table('tags')->skip(0)->limit(5)->lists('id');
+
+        foreach ($posts_id_arr as $each_post) {
+            foreach ($tags_id_arr as $each_tag) {
+                if ($each_tag > $each_post) {
+                    $this->insert_record($each_post, $each_tag);
+                }
+            }
+        }
+    }
+
+    /**
+     * insert the record of the table post_tag
+     * @param int $post_id
+     * @param int $tag_id
+     */
+    private function insert_record($post_id, $tag_id)
+    {
+        DB::table('post_tag')->insert([
+            'post_id' => $post_id,
+            'tag_id' => $tag_id,
+            'created_at' => date_create(),
+        ]);
+
     }
 }
